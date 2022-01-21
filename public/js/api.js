cd1b67cf3d95498d0caacfd2387e71e636925f2c
Nicole Pingar <nicolepingar@yahoo.com>
2022-01-21 11:32:22 -0500

    recipe buttons showing up on click

--- public/js/api.js
+++ public/js/api.js
@@ -1,5 +1,3 @@
-
-
 function formSubmit(event) {
     event.preventDefault();
     var mealInput = document.querySelector('#ingredient').value;
@@ -8,16 +6,26 @@ function formSubmit(event) {
 
 function getMeal(mealInput) {
     var mealDBURL = "https://www.themealdb.com/api/json/v1/1/search.php?f=" + mealInput;
-    console.log(mealDBURL);
     fetch(mealDBURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            console.log(data.meals);
+            for (let i = 0; i < data.meals.length; i++) {
+                const recipeName = data.meals[i].strMeal;
+                const mealButton = document.createElement('button');
+                const breakVar = document.createElement('div');
+                mealButton.innerHTML = "Click to add " + recipeName + " to the database.";
+                var recipesAppend = document.querySelector('.recipes');
+                breakVar.appendChild(mealButton)
+                recipesAppend.appendChild(breakVar);
+            }
         })
 };
 
 document
     .querySelector('.form-submit')
     .addEventListener('click', formSubmit);
+
+    //!!!!6.21 for appending 
