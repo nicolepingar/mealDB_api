f93738c3518989fa6421698861c96d7490b31b44
Nicole Pingar <nicolepingar@yahoo.com>
2022-01-21 10:47:08 -0500

    api pulling based on letter

--- public/js/api.js
+++ public/js/api.js
@@ -1,1 +1,23 @@
-const mealDB = "www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast"
+
+
+function formSubmit(event) {
+    event.preventDefault();
+    var mealInput = document.querySelector('#ingredient').value;
+    getMeal(mealInput);
+}
+
+function getMeal(mealInput) {
+    var mealDBURL = "https://www.themealdb.com/api/json/v1/1/search.php?f=" + mealInput;
+    console.log(mealDBURL);
+    fetch(mealDBURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+};
+
+document
+    .querySelector('.form-submit')
+    .addEventListener('click', formSubmit);
