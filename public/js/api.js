660decab65c662390f37ae0ad56243c696eee553
Nicole Pingar <nicolepingar@yahoo.com>
2022-01-22 10:49:07 -0500

    added comments to api.js and started recpe model

--- public/js/api.js
+++ public/js/api.js
@@ -1,31 +1,30 @@
+// when forms submitted, assign letter value to mealInput variable and call getMeal function with parameter
 function formSubmit(event) {
     event.preventDefault();
     var mealInput = document.querySelector('#ingredient').value;
     getMeal(mealInput);
 }
-
+// function to fetch MealDB api
 function getMeal(mealInput) {
-    var mealDBURL = "https://www.themealdb.com/api/json/v1/1/search.php?f=" + mealInput;
-    fetch(mealDBURL)
+    var mealDBURL = "https://www.themealdb.com/api/json/v1/1/search.php?f=" + mealInput; // add letter assigned to mealInput variable to end of API link
+    fetch(mealDBURL) // fetch API
         .then(function (response) {
-            return response.json();
+            return response.json(); // return in json
         })
-        .then(function (data) {
+        .then(function (data) { // data = API data
             console.log(data.meals);
-            for (let i = 0; i < data.meals.length; i++) {
+            for (let i = 0; i < data.meals.length; i++) { // loop through API data to create a button for each recipe
                 const recipeName = data.meals[i].strMeal;
                 const mealButton = document.createElement('button');
                 const breakVar = document.createElement('div');
                 mealButton.innerHTML = "Click to add " + recipeName + " to the database.";
                 var recipesAppend = document.querySelector('.recipes');
-                breakVar.appendChild(mealButton)
+                breakVar.appendChild(mealButton);
                 recipesAppend.appendChild(breakVar);
             }
         })
 };
-
+// submit button on click, call formSubmit function
 document
     .querySelector('.form-submit')
     .addEventListener('click', formSubmit);
-
-    //!!!!6.21 for appending 
