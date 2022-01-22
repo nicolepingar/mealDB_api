2298cbb632917c963ced2a471362e865e17c0093
Nicole Pingar <nicolepingar@yahoo.com>
2022-01-22 11:19:46 -0500

    recipe model almost completed

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,3 +2,53 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Recipe extends Model { }
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: false,
+        },
+        area: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ingredients: { //!!!
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        instructions: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        image: { //!!
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        measurements: { //!!
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe',
+    }
+);
+
+module.exports = Recipe;
+
+// area, category, ingredients? (up to 20), instructions, meal (name), meal thumb, measurements
