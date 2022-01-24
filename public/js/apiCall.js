6de0b901f26d519d80d8c80728b27939d0dd2fd6
Nicole Pingar <nicolepingar@yahoo.com>
2022-01-24 12:38:22 -0500

    individual bottons working

--- public/js/apiCall.js
+++ public/js/apiCall.js
@@ -15,32 +15,71 @@ function getMeal(mealInput) {
             console.log(data.meals);
             for (let i = 0; i < data.meals.length; i++) { // loop through API data to create a button for each recipe
                 const recipeName = data.meals[i].strMeal;
-                const id = data.meals[i].idMeal;
+                const recipeId = data.meals[i].idMeal;
                 const mealButton = document.createElement('button');
                 const breakVar = document.createElement('div');
                 mealButton.innerHTML = "Click to add " + recipeName + " to the database.";
-                mealButton.setAttribute('data-id', id);
+                mealButton.setAttribute('data-id', recipeId);
                 mealButton.setAttribute('class', `addButton`);
                 var recipesAppend = document.querySelector('.recipes');
                 breakVar.appendChild(mealButton);
                 recipesAppend.appendChild(breakVar);
 
-                const addButtonHandler = (event) => {
-                    if (event.target.hasAttribute('data-id')) {
-                        const idButton = event.target.getAttribute('data-id');
-                        console.log(idButton);
-                    }
-                };
 
-                // loops through all buttons with "addButton" class 
-                const addButton = document.getElementsByClassName('addButton');
-                for (let j = 0; j < addButton.length; j++) {
-                    addButton[j].addEventListener('click', addButtonHandler);
-                }
+
+
+
             }
+            const addButtonHandler = (event) => {
+                event.preventDefault();
+                if (event.target.hasAttribute('data-id')) {
+                    const idButton = event.target.getAttribute('data-id');
+                    console.log("idButton", idButton);
+                    let idk = ''
+                    let index = ''
+                    for (let i = 0; i < data.meals.length; i++) {
+                        if (data.meals[i].idMeal === idButton) {
+                            idk = data.meals[i].idMeal;
+                            index = i;
+                        }
+                    }
+                    console.log("idk", idk);
+                    const id = idk;
+                    const area = data.meals[i].strArea;
+                    const category = data.meals[i].strCategory;
+                    const instructions = data.meals[i].strInstructions;
+                    const name = data.meals[i].strMeal;
+                    const image = data.meals[i].strMealThumb;
+                    const ingredients1 = (data.meals[i].strMeasure1, data.meals[i].strIngredient1);
+
+
+                    // const response = await fetch('/api/recipes', {
+                    //     method: 'POST',
+                    //     body: JSON.stringify({ id, area, category, instructions, name, image, ingredients1 }),
+                    //     headers: {
+                    //         'Content-Type': 'application/json',
+                    //     },
+                    // });
+                    // if (response.ok) {
+                    //     document.location.reload();
+                    // } else {
+                    //     alert('Failed to add recipe.');
+                    // }
+                }
+            };
+            // loops through all buttons with "addButton" class 
+            const addButton = document.querySelectorAll('button');
+            // for (let j = 0; j < addButton.length; j++) {
+            //     addButton[j].addEventListener('click', addButtonHandler);
+            // }
+            addButton.forEach(element => {
+                element.addEventListener('click', addButtonHandler);
+            });
+            // document.getElementsByClassName(`addButton${recipeId}`).addEventListener('click', addButtonHandler)
         })
 };
 // submit button on click, call formSubmit function
 document
     .querySelector('.form-submit')
     .addEventListener('click', formSubmit);
+
