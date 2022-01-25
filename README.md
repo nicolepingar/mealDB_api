341df2c3bfa9ee0988c1d9773ae5b674da13d5f5
Nicole Pingar <nicolepingar@yahoo.com>
2022-01-25 10:55:02 -0500

    added more comments and completed readme/documentation

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # MealDB_API
 
+Application link: https://secret-atoll-62245.herokuapp.com/
 
+## Description
 
+This application uses MealDB API to load meal recipes alphabetically. Users can store selected recipes in the database and search based on specific ingredients. 
+
+## Authentication
+
+1. Search for recipes alphabetically.
+2. Add recipes to database by clicking on recipe button.
+3. Click "Search Database" in navigation bar to navigate to Database API. 
+4. Search for recipes via ingredients such as "chicken" or "asparagus."
+
+## Questions
+
+Please find me on GitHub [nicolepingar](https://github.com/nicolepingar) or email me at nicolepingar@gmail.com.
--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -3,7 +3,8 @@ const Recipe = require('../../models/Recipe');
 // add a recipe to the database
 router.post('/', async (req, res) => {
     try {
-        const recipeData = await Recipe.create({
+        const recipeData = await Recipe.create({ // adds recipe to "recipe" table in database 
+            // come from HTTP request body in "apiCall.js"
             id: req.body.id,
             area: req.body.area,
             category: req.body.category,
@@ -12,9 +13,9 @@ router.post('/', async (req, res) => {
             image: req.body.image,
             ingredients1: req.body.ingredients1
         })
-        res.status(200).json(recipeData)
+        res.status(200).json(recipeData) // if status is 200 (good) convert recipe data to json
     }
-    catch (err) {
+    catch (err) { //if there's an error, print error in console log
         console.log(err);
         res.status(400).json(err);
     }
